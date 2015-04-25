0db2b2caa5f1a848bba49e6f5bf77b6bd829f7bb
Louis van Beurden <louis.van-beurden@ensimag.grenoble-inp.fr>
2015-04-25 22:12:24 +0200

    lo

--- ajoutDonnees.sql
+++ ajoutDonnees.sql
@@ -1,57 +1,57 @@
 --Ajout de quelques producteurs
 INSERT INTO Compte (email, mdp) VALUES
-('Producteur1@gmail.com','35f2cdb8924e8fcd12b86ad621304792');
+('Producteur1@gmail.com','750f7eda495bfdb2e6c52c5b7d767da4f33e2633683373a2f914e96c0a7407a8');
 INSERT INTO Utilisateur (idUtilisateur, prenom, nom, adresse) VALUES
-((SELECT MAX(idCompte) FROM Compte), 'prenomProd1', 'nomProd1', 'adresseProd1');
+((SELECT MAX(idCompte) FROM Compte), 'Jean', 'Ulrich', '10 rue Gabriel Faure Grenoble');
 INSERT INTO Producteur (idProducteur, jourLivraison) VALUES
 ((SELECT MAX(idCompte) FROM Compte), 'Lundi');
 
 INSERT INTO Compte (email, mdp) VALUES
-('Producteur2@gmail.com','b249561af7904727c4bceefed868c373');
+('Producteur2@gmail.com','d30c129a9fd5d36f098de00711066f9566aa1922e7296e3bab783d74edb61a64');
 INSERT INTO Utilisateur (idUtilisateur, prenom, nom, adresse) VALUES
-((SELECT MAX(idCompte) FROM Compte), 'prenomProd2', 'nomProd2', 'adresseProd2');
+((SELECT MAX(idCompte) FROM Compte), 'Salim', 'Guerande', '5 bis avenue Leon Blum Paris');
 INSERT INTO Producteur (idProducteur, jourLivraison) VALUES
 ((SELECT MAX(idCompte) FROM Compte), 'Mercredi');
 
 INSERT INTO Compte (email, mdp) VALUES
-('Producteur3@gmail.com','0bd69f1a8211f0ffc81216e5de6e3e77');
+('Producteur3@gmail.com','bf61d3d0d22c65a86d72025d2d668b27c25426f864fd0459b543a151092369f8');
 INSERT INTO Utilisateur (idUtilisateur, prenom, nom, adresse) VALUES
-((SELECT MAX(idCompte) FROM Compte), 'prenomProd3', 'nomProd3', 'adresseProd3');
+((SELECT MAX(idCompte) FROM Compte), 'Jean-Joseph', 'Placardeau', '9 place du Roi Versailles');
 INSERT INTO Producteur (idProducteur, jourLivraison) VALUES
 ((SELECT MAX(idCompte) FROM Compte), 'Jeudi');
 
 INSERT INTO Compte (email, mdp) VALUES
-('Producteur4@gmail.com','10022410286caf49eaf022a062ef45e1');
+('Producteur4@gmail.com','490fbe4167e975596ce088c4f6bb04265e2fce368c51ba56fe178288ef3d6359');
 INSERT INTO Utilisateur (idUtilisateur, prenom, nom, adresse) VALUES
-((SELECT MAX(idCompte) FROM Compte), 'prenomProd4', 'nomProd4', 'adresseProd4');
+((SELECT MAX(idCompte) FROM Compte), 'Jauselie', 'Cauctaille', '987 Wallabie Sydney');
 INSERT INTO Producteur (idProducteur, jourLivraison) VALUES
 ((SELECT MAX(idCompte) FROM Compte), 'Dimanche');
 
 INSERT INTO Compte (email, mdp) VALUES
-('b@b','b');
+('b@b','3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d');
 INSERT INTO Utilisateur (idUtilisateur, prenom, nom, adresse) VALUES
-((SELECT MAX(idCompte) FROM Compte), 'b', 'b', 'b');
+((SELECT MAX(idCompte) FROM Compte), 'Yves', 'La Roche', '5 ter rue des Non Dit Carpentra');
 INSERT INTO Producteur (idProducteur, jourLivraison) VALUES
 ((SELECT MAX(idCompte) FROM Compte), 'Lundi');
 
 --Ajout des utilisateurs
 INSERT INTO Compte (email, mdp) VALUES
-('a@a','a');
+('a@a','ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb');
 INSERT INTO Utilisateur (idUtilisateur, prenom, nom, adresse) VALUES
-((SELECT MAX(idCompte) FROM Compte), 'a', 'a', 'a');
+((SELECT MAX(idCompte) FROM Compte), 'Mathilde', 'Dufeux', '48 avenue des Pierre Echirolles');
 INSERT INTO Consommateur (idConsommateur) VALUES
 ((SELECT MAX(idCompte) FROM Compte));
 
 INSERT INTO Compte (email, mdp) VALUES
-('d@d','d');
+('d@d','18ac3e7343f016890c510e93f935261169d9e3f565436429830faf0934f4f8e4');
 INSERT INTO Utilisateur (idUtilisateur, prenom, nom, adresse) VALUES
-((SELECT MAX(idCompte) FROM Compte), 'd', 'd', 'd');
+((SELECT MAX(idCompte) FROM Compte), 'Lea', 'Prulaux', '20 boulevard Farouche Esteralle');
 INSERT INTO Consommateur (idConsommateur) VALUES
 ((SELECT MAX(idCompte) FROM Compte));
 
 --Ajout d'un respo planning
 INSERT INTO Compte (email, mdp) VALUES
-('c@c','c');
+('c@c','2e7d2c03a9507ae265ecf5b5356885a53393a2029d241394997265a1a25aefc6');
 INSERT INTO ResponsablePlanning (idRespo) VALUES
 ((SELECT MAX(idCompte) FROM Compte));
 
--- mvn_project/src/main/java/controleur/AuthorisationManager.java
+++ mvn_project/src/main/java/controleur/AuthorisationManager.java
@@ -1,6 +1,7 @@
 package controleur;
 
 import java.io.IOException;
+import java.security.MessageDigest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,59 +19,77 @@ public class AuthorisationManager {
 
 	public static final String CREDENTIAL_SESSION_VAR = "credential";
 	public static final String RETURN_SESSION_VAR = "return_page"; 
-	
+
 	public static boolean getPermission(HttpServletRequest request, HttpServletResponse response, Permission level) throws IOException {
 		HttpSession session = request.getSession(true);
-		
+
 		Credential credential = (Credential) session.getAttribute(CREDENTIAL_SESSION_VAR);
-		
+
 		if(credential == null) {
 			session.setAttribute(RETURN_SESSION_VAR, request.getRequestURI());
-			
+
 			response.sendRedirect("/caweb/authentification");
 			return false;
 		}
-		
+
 		if(!credential.isAllowed(level)) {
 			response.sendRedirect("/caweb");
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
+	private static String hashString(String mdp) {
+		try{
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(mdp.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer();
+
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+
+			return hexString.toString();
+		} catch(Exception ex){
+			throw new RuntimeException(ex);
+		}
+	}
+
 	public static boolean haveCredential(HttpSession session) {
 		return session.getAttribute(CREDENTIAL_SESSION_VAR) != null;
 	}
-	
+
 	public static boolean havePermission(HttpSession session, Permission permission) {
 		Credential cred = (Credential) session.getAttribute(CREDENTIAL_SESSION_VAR);
 		return cred != null && cred.isAllowed(permission);
 	}
-	
-	
+
+
 	public static boolean logSession(DataSource ds, HttpSession session, String email, String password) {
 		Compte compte = null;
-		
+
 		try {
-			compte = new CompteDAO(ds).getCompte(email, password);
+			compte = new CompteDAO(ds).getCompte(email, hashString(password));
 		} catch (DAOException e) {
 			e.printStackTrace();
 			return false;
 		}
-		
+
 		if(compte == null) {
 			return false;
 		}
-		
+
 		session.setAttribute(CREDENTIAL_SESSION_VAR, new Credential(compte));
 		return true;
 	}
-	
+
 	public static void unlogSession(HttpSession session) {
 		session.setAttribute(CREDENTIAL_SESSION_VAR, null);
 	}
-	
+
 	public static class AucunCompteLoggeException extends Exception {
 		private static final long serialVersionUID = 1L;
 	}
@@ -79,49 +98,49 @@ public class AuthorisationManager {
 		Credential credential = (Credential) session.getAttribute(CREDENTIAL_SESSION_VAR);
 		if(credential == null)
 			throw new AucunCompteLoggeException();
-		
+
 		return credential.idCompte;
 	}
-	
+
 	public enum Permission {
 		CONSOMMATEUR(0),
 		PRODUCTEUR(1),
 		RESPONSABLE_PLANNING(2);
-		
+
 		public int level;
-		
+
 		private Permission(int level) {
 			this.level = level;
 		}
 	}
-	
+
 	static public class Credential {
 
 		private Permission authorisation;
 		private int idCompte;
-		
+
 		private Permission getPermission(Compte compte) {
 			if(compte instanceof Consommateur)
 				return Permission.CONSOMMATEUR;
-			
+
 			if(compte instanceof Producteur)
 				return Permission.PRODUCTEUR;
-			
+
 			if(compte instanceof ResponsablePlanning)
 				return Permission.RESPONSABLE_PLANNING;
-			
+
 			throw new InternalError();
 		}
-		
+
 		private Credential(Compte compte) {
 			this.authorisation = getPermission(compte);
 			idCompte = compte.getId();
 		}
-		
+
 		public int getIdCompte() {
 			return idCompte;
 		}
-		
+
 		public Permission getAuthorisation() {
 			return authorisation;
 		}
